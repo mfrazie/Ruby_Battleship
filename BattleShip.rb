87da8195ea28df3ed2decaa51c3f00759b7b6fb0
Myles <Mfrazier056@gmail.com>
2019-07-12 15:19:51 -0400

    Initial

---
+++ BattleShip.rb
@@ -0,0 +1,4 @@
+#hash tables for [grid[],ships guessed]    
+# hash={}set up table
+#(name of hash)[:one]declare a index on table  >> hash now contains hash{:one} >> :one is a bookmark
+# puts hash[:one]    >> To Recall  that bookmark
